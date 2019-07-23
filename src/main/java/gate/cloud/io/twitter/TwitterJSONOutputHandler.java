7eb8469a82681f776b144fcdb8e82241edac79f8
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2019-07-23 09:41:35 +0100

    total re-write of the GCP output handler that actually seems to work

--- src/main/java/gate/cloud/io/twitter/TwitterJSONOutputHandler.java
+++ src/main/java/gate/cloud/io/twitter/TwitterJSONOutputHandler.java
@@ -5,55 +5,38 @@ import static gate.cloud.io.IOConstants.PARAM_FILE_EXTENSION;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.io.SerializedString;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonGenerator.Feature;
 
+import gate.Annotation;
 import gate.AnnotationSet;
 import gate.Document;
 import gate.Factory;
 import gate.FeatureMap;
 import gate.GateConstants;
-import gate.cloud.batch.AnnotationSetDefinition;
+import gate.Utils;
+import gate.annotation.AnnotationSetImpl;
 import gate.cloud.batch.DocumentID;
-import gate.cloud.io.OutputHandler;
 import gate.cloud.io.file.AbstractFileOutputHandler;
+import gate.corpora.DocumentJsonUtils;
 import gate.corpora.export.TwitterJsonExporter;
 import gate.util.GateException;
+import gate.util.OffsetComparator;
 
-public class TwitterJSONOutputHandler extends AbstractFileOutputHandler implements InvocationHandler {
+public class TwitterJSONOutputHandler extends AbstractFileOutputHandler {
 
 	private TwitterJsonExporter exporter = new TwitterJsonExporter();
 
-	protected static final ObjectMapper MAPPER = new ObjectMapper();
-
-	private OutputHandler jsonOutputHandler, jsonProxyHandler;
-
-	public TwitterJSONOutputHandler() {
-		try {
-			// because we don't publush the GCP Impl classes as a maven artifact we can't
-			// reference the JSONOutputHandler directly and can't extended it as an
-			// anonymous inner class to override the getFileOutputStream so...
-
-			// access the class by reflection
-			jsonOutputHandler = (OutputHandler) (Class.forName("gate.cloud.io.file.JSONOutputHandler").newInstance());
-			
-			// create a proxy we will use to allow us to change the instances behaviour
-			jsonProxyHandler = (OutputHandler) Proxy.newProxyInstance(this.getClass().getClassLoader(),
-					new Class<?>[] { OutputHandler.class }, this);
-
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			throw new RuntimeException("Can't find the standard JSON output handler", e);
-		}
-	}
+	private static final JsonFactory JSON_FACTORY = new JsonFactory().enable(Feature.AUTO_CLOSE_TARGET);
 
 	@Override
 	protected void configImpl(Map<String, String> configData) throws IOException, GateException {
@@ -63,67 +46,76 @@ public class TwitterJSONOutputHandler extends AbstractFileOutputHandler implemen
 		}
 
 		super.configImpl(configData);
-
-		// configure the underlying JSON output handler correctly with the assumption
-		// that the document annotation is Tweet in the Original markups annotaiton set
-		Map<String, String> jsonConfig = new HashMap<String, String>(configData);
-		jsonConfig.put("groupEntitiesBy", "entities");
-		jsonConfig.put("documentAnnotationASName", GateConstants.ORIGINAL_MARKUPS_ANNOT_SET_NAME);
-		jsonConfig.put("documentAnnotationType", "Tweet");
-		jsonProxyHandler.config(jsonConfig);
 	}
 
 	@Override
 	protected void outputDocumentImpl(Document document, DocumentID documentId) throws IOException, GateException {
 
+		Map<String, Collection<Annotation>> annotationSetsMap = collectAnnotations(document);
+
+		Map<String, Collection<Annotation>> originalMap = annotationSetsMap;
+		annotationSetsMap = new HashMap<String, Collection<Annotation>>();
+		for (Collection<Annotation> annSet : originalMap.values()) {
+			for (Annotation a : annSet) {
+				Collection<Annotation> annsByType = annotationSetsMap.get(a.getType());
+				if (annsByType == null) {
+					annsByType = new AnnotationSetImpl(document);
+					annotationSetsMap.put(a.getType(), annsByType);
+				}
+				annsByType.add(a);
+			}
+		}
+
 		AnnotationSet originalMarkups = document.getAnnotations(GateConstants.ORIGINAL_MARKUPS_ANNOT_SET_NAME);
 
-		if (originalMarkups.get("TweetSegment").isEmpty()) {
-			// if there are no TweetSegment annotations fall back to the normal JSON handler
-			// pre-configured to treat Tweet in Original markups as the document
-			jsonProxyHandler.setAnnSetDefinitions(getAnnSetDefinitions());
-			jsonProxyHandler.outputDocument(document, documentId);
-		} else {
-			// we have an actual tweet object we can do something with
+		OutputStream outputStream = getFileOutputStream(documentId);
+		OutputStreamWriter writer = new OutputStreamWriter(outputStream,
+				(encoding == null || encoding.length() == 0 ? "UTF-8" : encoding));
+		JsonGenerator generator = JSON_FACTORY.createGenerator(writer);
 
-			OutputStream outputStream = getFileOutputStream(documentId);
+		try {
 
-			// create the JSON generator we want to use
-			JsonGenerator generator = MAPPER.getFactory()
-					.createGenerator(new OutputStreamWriter(outputStream, "UTF-8"));
-			generator.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
-			generator.enable(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT);
-			generator.setRootValueSeparator(new SerializedString("\n"));
-
-			// Convert all the annoation set descriptions into a simple set of Strings where
-			// set and type are separated by :
-			Set<String> annotationTypes = new HashSet<String>();
-			for (AnnotationSetDefinition definition : getAnnSetDefinitions()) {
-				for (String type : definition.getAnnotationTypes()) {
-					annotationTypes.add(definition.getAnnotationSetName() + ":" + type);
+			if (originalMarkups.get("TweetSegment").isEmpty()) {
+				Comparator<Annotation> comparator = new OffsetComparator();
+				for (Map.Entry<String,Collection<Annotation>> entry : annotationSetsMap.entrySet()) {
+					List<Annotation> list = new ArrayList<Annotation>();
+					list.addAll(entry.getValue());
+					Collections.sort(list, comparator);
+					entry.setValue(list);
 				}
+				
+				AnnotationSet documentAnnotationSet = originalMarkups.get("Tweet");
+				if (documentAnnotationSet.size() > 1) {
+					throw new GateException("Found more than one Tweet annotation for document " + documentId);
+				}
+				if (documentAnnotationSet.size() > 0) {
+					Annotation documentAnnotation = Utils.getOnlyAnn(documentAnnotationSet);
+					DocumentJsonUtils.writeDocument(document, Utils.start(documentAnnotation),
+							Utils.end(documentAnnotation), annotationSetsMap, documentAnnotation.getFeatures(), null,
+							generator);
+					return;
+				}
+
+				// if we get here we either didn't have documentAnnotationType
+				// set, or it was set but the document contained no such
+				// annotation - simply output the whole document with no extra
+				// features.
+				DocumentJsonUtils.writeDocument(document, 0L, Utils.end(document), annotationSetsMap, null, null,
+						generator);
+
+			} else {
+				// we have an actual tweet object we can do something with
+
+				// create a FeatureMap of options to drive the underlying Twitter exporter
+				FeatureMap options = Factory.newFeatureMap();
+				options.put("annotationsMap", annotationSetsMap);
+
+				// export the document
+				exporter.export(document, generator, options);
 			}
 
-			// create a FeatureMap of options to drive the underlying Twitter exporter
-			FeatureMap options = Factory.newFeatureMap();
-			options.put("annotationTypes", annotationTypes);
-
-			// export the document
-			exporter.export(document, generator, options);
+		} finally {
+			generator.close();
 		}
-	}
-
-	@Override
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		// this is called when methods on the underlying JSON output handler are called.
-		// In most cases we just proxy the call through, but if the call is to
-		// getFileOutpuStream then instead we return the call to the same mehtod on this
-		// class, ensuring that everything goes into the same file. Very important in
-		// the case where we extend this handler to support streaming output
-
-		if (method.getName().equals("getFileOutputStream"))
-			return getFileOutputStream((DocumentID) args[0]);
-
-		return method.invoke(jsonOutputHandler, args);
 	}
 }
